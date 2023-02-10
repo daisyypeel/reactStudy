97c8ef4293fbd2a3b96d59355334430d2832b992
daisy19 <daisy@adnstyle.com>
2023-02-10 17:07:52 +0900

    해결못함

--- new-nomad/src/CoinTracker.js
+++ new-nomad/src/CoinTracker.js
@@ -3,6 +3,20 @@ import { useEffect, useState } from "react";
 function CoinTracker() {
 	const [loading, setLoading] = useState(true);
 	const [coins, setCoins] = useState([]);
+	const [asset, setAsset] = useState("");
+	const [exchange, setExchange] = useState(0);
+
+	const onChange = (event) => setAsset(event.target.value);
+	const onSubmit = (event) => {
+		event.preventDefault();
+		
+		setAsset("");
+		setExchange((currentAsset) => (asset));
+	}
+
+	console.log(asset)
+	console.log(exchange)
+
 	useEffect(() => {
 		fetch("https://api.coinpaprika.com/v1/tickers")
 			.then((response) => response
@@ -14,14 +28,17 @@ function CoinTracker() {
 	return (
 		<div>
 			<h1>The Coins! {loading ? "" : `(${coins.length})`} </h1>
-			<input type="text"></input>
-			<br/><br/>
 			{loading ? (<strong>Loading...</strong>) : (
 				<select>
 					{coins.map((coin) => <option key={coin.id}>{coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD</option>)}
 				</select>
 			)}
 
+			<br/><br/>
+			<form onSubmit={onSubmit}>
+				<input onChange={onChange} value={asset} type="number" placeholder="write your asset" /> USD
+				<hr/>
+			</form>
 		</div>
 	);
 }
