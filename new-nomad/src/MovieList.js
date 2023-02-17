f719348bce468f7f4f3fa843d88590a1abf955be
daisy19 <daisy@adnstyle.com>
2023-02-17 11:28:20 +0900

    새로고침 오류 해결

--- new-nomad/src/MovieList.js
+++ new-nomad/src/MovieList.js
@@ -9,12 +9,13 @@ import Detail from "./routes/Detail";
 
 function MovieList() {
 	return (
-		<Router>
-			<Routes basename={process.env.PUBLIC_URL}>
+		<Router basename={process.env.PUBLIC_URL}>
+			<Routes>
 				<Route path={`${process.env.PUBLIC_URL}/movie/:id`} element={<Detail />} />
-				<Route path={`${process.env.PUBLIC_URL}/`} element={<Home />} />
+				<Route path="/" element={<Home />} />
 			</Routes>
 		</Router>
+
 	);
 }
 
